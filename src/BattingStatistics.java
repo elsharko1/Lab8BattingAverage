82d69f1f73397e573dce37b78d3a8458ddc10218
elsharko1 <elsharko1@gmail.com>
2017-07-06 19:53:08 -0400

    thursday night after class

--- src/BattingStatistics.java
+++ src/BattingStatistics.java
@@ -9,34 +9,62 @@ public class BattingStatistics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int atBats = 0;
+        String choice = "y";
+
         //double basesEarned = 0;
+        while(choice.equalsIgnoreCase("y")) {
+            atBats = numBatsEntered(scan);
 
-        System.out.println("Please enter the number of At-bats: ");
-            atBats = scan.nextInt();
-            while (atBats < 0){
-                System.out.println("Invalid input, please enter a valid number: ");
-                atBats = scan.nextInt();
+            int[] arrSize = new int[atBats];
+            //System.out.println(arrSize.length);
+            basesEarned(scan, arrSize);
 
-            }
-
-        int[] arrSize = new int[atBats];
-
-        //System.out.println(arrSize.length);
-
-        for (int i = 0; i < arrSize.length; i++) {
-            System.out.println("Please enter number of bases earned for each At-Bat " + (i + 1) + ": ");
-            //variable for userInput
-            System.out.println("0 - Strike-out" + "1 - single" + "2 - double" + "3 - triple" + "4 - homerun");
-            int userNum = scan.nextInt();
-            arrSize[i] = userNum;
-            //validation for numbers entered
-            while(userNum > 4 | userNum < 0){
-                System.out.println("Invalid input, please enter a valid number:");
-                userNum = scan.nextInt();
-            }
+            batSlugsNum(atBats, arrSize);
+            
+            scan.nextLine();
+            System.out.println();
+            System.out.println("Would you like to continue:");
+            System.out.println("Please enter Y or N: ");
+            choice = scan.nextLine();
 
         }
+        System.out.println("Good Bye!!!");
+/*Jagged 2D example
+        int[][] arr2 = new int[5][];
+        //arr2[0][0] = 34;
+        //arr2[1][0] = 55;
 
+        //try to line up values so you can see it easier
+        int[][] numbers = {{1, 2, 4, 6, 7},
+                {4, 6, 8, 9, 12, 45},
+                {56, 65}};
+
+        int sum = 0;
+        for (int [] row: numbers){
+            //this is an array of arrays
+            //set an array for each type then move through them
+            for(int column : row){
+                sum += column;
+
+            }
+            */
+
+        /*//prompt user to enter a numnber based on size of array they gave me
+        //assigning user entered values to the index positions
+       private static void fillArray(Scanner scan, int[] arr) {
+           for (int i = 0; i < arr.length; i++) {
+            System.out.println("Please enter a number for index " + i +": ");
+            //variable for userInput
+            int userNum = scan.nextInt();
+            arr[i] = userNum;
+
+        }
+        */
+
+
+    }
+
+    private static void batSlugsNum(int atBats, int[] arrSize) {
         double actualHits = 0;//hits variable is for batting average, will ignore 0
         for (int i = 0; i < arrSize.length; i++) {
             //this will ignore strike outs because it will ignore 0.
@@ -84,40 +112,33 @@ public class BattingStatistics {
         //with slugging percentage
 
         //need a bases earned variable that goes along with the times at bat variable
+    }
 
-
-/*Jagged 2D example
-        int[][] arr2 = new int[5][];
-        //arr2[0][0] = 34;
-        //arr2[1][0] = 55;
-
-        //try to line up values so you can see it easier
-        int[][] numbers = {{1, 2, 4, 6, 7},
-                {4, 6, 8, 9, 12, 45},
-                {56, 65}};
-
-        int sum = 0;
-        for (int [] row: numbers){
-            //this is an array of arrays
-            //set an array for each type then move through them
-            for(int column : row){
-                sum += column;
-
-            }
-            */
-
-        /*//prompt user to enter a numnber based on size of array they gave me
-        //assigning user entered values to the index positions
-       private static void fillArray(Scanner scan, int[] arr) {
-           for (int i = 0; i < arr.length; i++) {
-            System.out.println("Please enter a number for index " + i +": ");
+    private static void basesEarned(Scanner scan, int[] arrSize) {
+        for (int i = 0; i < arrSize.length; i++) {
+            System.out.println("Please enter number of bases earned for each At-Bat " + (i + 1) + ": ");
             //variable for userInput
+            System.out.println("0 - Strike-out" + "1 - single" + "2 - double" + "3 - triple" + "4 - homerun");
             int userNum = scan.nextInt();
-            arr[i] = userNum;
+            arrSize[i] = userNum;
+            //validation for numbers entered
+            while(userNum > 4 | userNum < 0){
+                System.out.println("Invalid input, please enter a valid number:");
+                userNum = scan.nextInt();
+            }
 
         }
-        */
+    }
 
+    private static int numBatsEntered(Scanner scan) {
+        int atBats;
+        System.out.println("Please enter the number of At-bats: ");
+        atBats = scan.nextInt();
+        while (atBats < 0){
+            System.out.println("Invalid input, please enter a valid number: ");
+            atBats = scan.nextInt();
 
+        }
+        return atBats;
     }
 }
